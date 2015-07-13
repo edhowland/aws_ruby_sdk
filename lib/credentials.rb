ec9f8afb68afd92bce38bc8227e2b2fe4c6740d1
edhowland <ed.howland@gmail.com>
2015-07-13 15:37:19 +0000

    \'2 failures in credentials_spec.rb\'

--- lib/credentials.rb
+++ lib/credentials.rb
@@ -1,1 +1,10 @@
 # credentials.rb - methodload_credentials
+
+# load_credentials_via_require - require file of credentials from outside this source tree
+def load_credentials_via_require
+end
+
+# load_credentials - selects which method to use
+def load_credentials
+  load_credentials_via_require
+end
--- spec/credentials_spec.rb
+++ spec/credentials_spec.rb
@@ -3,9 +3,11 @@
 require_relative 'spec_helper'
 require 'credentials'
 require 'minitest/autorun'
-
+#  ENV['AWS_ACCESS_KEY_ID']
+#  ENV['AWS_SECRET_ACCESS_KEY'] 
 describe 'load_credentials' do
   before { load_credentials }
 
-  specify {ENV[].wont_be_nil }
+  specify {ENV['AWS_ACCESS_KEY_ID'].wont_be_nil }
+  specify { ENV['AWS_SECRET_ACCESS_KEY'].wont_be_nil } 
 end
