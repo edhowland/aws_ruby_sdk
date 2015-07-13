4c43ffa93b2ab0e8a0270dba0ed1563288a1f2d0
edhowland <ed.howland@gmail.com>
2015-07-13 20:09:36 +0000

    \'added ../lib/application.rb ../lib/query.rb\'

---
+++ lib/application.rb
@@ -0,0 +1,9 @@
+# application.rb - Loads all library files
+require 'aws-sdk-resources'
+require_relative 'configuration'
+require_relative 'configuration/region'
+require_relative 'query'
+
+# load regions and credentials
+load_region
+load_credentials
---
+++ lib/query.rb
@@ -0,0 +1,3 @@
+# query.rb - requires for query/
+
+require_relative 'query/ec2_query'
