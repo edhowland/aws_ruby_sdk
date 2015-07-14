12c7c5b6d8b8cd1d37e415ae225d67126053c7cd
edhowland <ed.howland@gmail.com>
2015-07-14 00:15:44 +0000

    \'checking S3 Buckets for query.rb\'

--- bin/query.rb
+++ bin/query.rb
@@ -3,7 +3,7 @@
 
 require '../lib/application'
 
-puts "Aquireing EC2 rsource in Region: #{ENV['AWS_REGION']}"
+puts "Acquireing EC2 rsource in Region: #{ENV['AWS_REGION']}"
 ec2 = ec2_resource
 
 puts 'Enumerating EC2 Instances'
@@ -17,5 +17,19 @@ end
 
 puts "Found #{instance_count} Instances"
 
+
+puts 'Acquiring S3 Rsources'
+
+s3 = s3_resource
+
+puts 'Enumerating Buckets'
+buckets = s3.buckets
+bucket_count = 0
+buckets.each do |bucket|
+  puts 'Found bucket:'
+  bucket_count += 1
+end
+
+puts "found #{bucket_count} S3 Buckets"
 
 puts 'Done'
