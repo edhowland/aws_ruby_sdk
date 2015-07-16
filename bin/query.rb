635908fc829001cedf5992eb07c86db913538c25
edhowland <ed.howland@gmail.com>
2015-07-16 00:43:33 +0000

    \'added found_options hash to check some option was given\'

--- bin/query.rb
+++ bin/query.rb
@@ -51,19 +51,33 @@ def list_s3_objects
 end
 
 # set any options on the command line
+found_options = {ec2: false, s3: false}
 options do|opts|
   opts.on('-e', '--list-ec2', 'Enumerates all EC2 instances in this region') do
+    found_options[:ec2] = true
     list_ec2_instances
   end
 
   opts.on('-s', '--list-s3', 'Enumerates S3 objects', ) do
     list_s3_objects
+    found_options[:s3] = true
   end
 
   opts.on('-a', '--list-all', 'Displays all queries') do
+    found_options[:a] = true # not a required option, but satisfys that some option was found
     list_ec2_instances
     list_s3_objects
   end
 end
 
-puts 'Done'
+unless found_options.values.reduce(false) {|i, j| i || j }
+  puts <<-EOP
+What do you want to query?
+Available options are:
+-e, --list-ec2: List all EC2 instances
+-s, --list-s3: List all S3 objects
+-a, --list-all: List everything
+EOP
+else
+  puts 'Done'
+end
