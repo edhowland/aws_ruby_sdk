f671042f2a26086b88805eed4cfb6e2ff36bc035
edhowland <ed.howland@gmail.com>
2015-07-17 01:43:17 +0000

    \'added key pair skeleton to ec2_instance.rb\'

--- bin/ec2_instance.rb
+++ bin/ec2_instance.rb
@@ -3,7 +3,13 @@
 
 require_relative '../lib/application'
 
+options('EC2 Instance Operations') do |opts|
+  option opts, :create_key, 'Create Key Pair', 'k' do
+    puts 'Creating key pair'
+  end
 
-options do |opts|
+  option opts, :delete_key, 'Delete Key Pair' do
+    puts'Deleteing Key Pair'
+  end
 
 end
