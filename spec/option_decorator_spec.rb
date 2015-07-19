7af0792f43186165788df367824deb4403988864
edhowland <ed.howland@gmail.com>
2015-07-19 17:57:55 +0000

    \'OptionDecorators using source line comment[A\'

--- spec/option_decorator_spec.rb
+++ spec/option_decorator_spec.rb
@@ -7,14 +7,18 @@ require 'minitest/autorun'
 class OptionDecorator < HandlerFramework
   def _end; end # need this to signal end of sublass method list
 
+  def eval_source source_and_line 
+    line = read_line *source_and_line
+    method_and_comment = line.split('#')
+      eval method_and_comment[1]
+  end
   def method_list
     handlers.map {|h| self.method(h) }
   end
 
-  # returns lines preceding each method def
   def decorators
-    method_list.map {|m| source = m.source_location; {m.name => read_line(source[0], source[1] )} }
-    method_list.reduce({}) {|i,  m| i[m.name] = eval(read_line(*m.source_location)) ; i }
+  #  method_list.map {|m| source = m.source_location; {m.name => read_line(source[0], source[1] )} }
+    method_list.reduce({}) {|i,  m| i[m.name] = eval_source(m.source_location); i }
   end
 
   def decorators_hash
@@ -36,6 +40,7 @@ describe OptionDecorator do
   end
 
   describe 'decorators' do
-    specify { opd.decorators.must_equal [{list_things: "  # { description: 'List Things' }"}] }
+    before { @h = {list_things: {description: 'List Things'} } }
+    specify { opd.decorators.must_equal @h }
   end
 end
