0f87e64de54696100af63494ee1194e0d1c72b71
edhowland <ed.howland@gmail.com>
2015-07-20 01:11:15 +0000

    \'modded query.rb to use OptionDecorator\'

--- bin/query.rb
+++ bin/query.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/env ruby
 # query.rb - Query AWS Resources
 
-require '../lib/application'
+require_relative  '../lib/application'
 require './messages'
 require './requestor'
 
 # All methods names matching option long names go here.
-class QueryRequestor < RequestFramework
-  def list_regions description='List U. S. Regions'
-    return yield description if block_given? 
-
-    puts description
+class QueryRequestor < OptionDecorator
+  def list_regions # {description: 'List U. S. Regions', short: 'r'}
+    puts <<-EOP
+us-east-1
+us-west-1
+us-west-2
+EOP
   end
 end
 
