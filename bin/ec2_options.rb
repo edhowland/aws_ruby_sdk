58f97fddcb97db13a038f0d91b9bf8559ef03f14
edhowland <ed.howland@gmail.com>
2015-07-21 16:10:27 +0000

    \'saving to json ec2_options.rb\'

--- bin/ec2_options.rb
+++ bin/ec2_options.rb
@@ -3,10 +3,15 @@
 class Ec2Options
   def initialize fname='ec2_default.json'
     @fname = fname
+    @options = {}
   end
 
   def self.load fname
     self.new fname
+  end
+
+  def save
+    File.write(@fname, @options.to_json)
   end
 end
 
