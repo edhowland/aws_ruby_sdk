ee9a074e715778362db63f2b2d000cb72bcf3e2d
edhowland <ed.howland@gmail.com>
2015-07-21 16:14:20 +0000

    \'loading json from file\'

--- bin/ec2_options.rb
+++ bin/ec2_options.rb
@@ -1,13 +1,14 @@
 # ec2_options.rb -Ec2Options
 
 class Ec2Options
-  def initialize fname='ec2_default.json'
+  def initialize fname='ec2_default.json', options={}
     @fname = fname
-    @options = {}
+    @options = options
   end
 
   def self.load fname
-    self.new fname
+    options = JSON.load(File.read(fname))
+    self.new fname, options
   end
 
   def save
