0c2f121ef32bc0d48e163fb07165afeed7ff93b1
edhowland <ed.howland@gmail.com>
2015-07-24 01:28:27 +0000

    \'moved describe instance to own method\'

--- bin/query.rb
+++ bin/query.rb
@@ -5,6 +5,21 @@ require_relative  '../lib/application'
 require './messages'
 require './requestor'
 
+
+def describe_instance instance
+        puts "Instance ID: #{instance.id}"
+  puts "\tInstance image: #{instance.image.id}"
+    puts "\tType: #{instance.instance_type}"
+    puts "\tKey Pair: #{instance.key_pair.key_name}"
+    puts "\tPrivate IP Address: #{instance.private_ip_address}"
+   puts "\tPublic IP: #{instance.public_ip_address}"
+    puts "\tVPC ID: #{instance.vpc_id}"
+    puts "\tPublic DNS: #{instance.public_dns_name}"
+    puts "\tState of the instance: #{instance.state.name}"
+    puts "\tState Reason: #{instance.state_reason.message}"
+end
+
+
 # All methods names matching option long names go here.
 # E.g --list-groups : -> def list_groups
 class QueryRequestor < OptionDecorator
@@ -67,18 +82,9 @@ EOP
     begin
       instances.each do |instance|
         puts 'Instance of EC2'
+    describe_instance instance
         instance_count += 1
-        puts "Instance ID: #{instance.id}"
-  puts "\tInstance image: #{instance.image.id}"
-    puts "\tType: #{instance.instance_type}"
-    puts "\tKey Pair: #{instance.key_pair.key_name}"
-    puts "\tPrivate IP Address: #{instance.private_ip_address}"
-   puts "\tPublic IP: #{instance.public_ip_address}"
-    puts "\tVPC ID: #{instance.vpc_id}"
-    puts "\tPublic DNS: #{instance.public_dns_name}"
-    puts "\tState of the instance: #{instance.state.name}"
-    puts "\tState Reason: #{instance.state_reason.message}"
-      end
+    end
     rescue => err
       puts "Enumerating EC2 instances raised error:"
       puts err.message
