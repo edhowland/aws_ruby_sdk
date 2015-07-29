c4e5b747459cfb5419033db8f826e5f3fb1e3dc5
edhowland <ed.howland@gmail.com>
2015-07-29 22:39:46 +0000

    \'got expand_short working under most circumstances\'

--- spec/expand_opt_spec.rb
+++ spec/expand_opt_spec.rb
@@ -5,7 +5,7 @@ require 'application'
 require 'minitest/autorun'
 
 def expand_short key, options={}
-  '-' + (options[:short] || key.to_s[0]) + (options[:arg] ? ' name' : '')
+  (options[:short] == :nop ? nil : ('-' + (options[:short] || key.to_s[0]) + (options[:arg] ? ' name' : '')))
 end
 
 describe 'expand_short' do
@@ -13,9 +13,11 @@ describe 'expand_short' do
   let(:override) { expand_short :option, {short: 'p'} }
   let(:oarg) { expand_short :option, {arg: String} }
   let(:arg_over) { expand_short :option, {short: 'p', arg: String} }
+  let(:noshort) { expand_short :option, {short: :nop} }
 
   specify { opt.must_equal '-o' }
   specify { override.must_equal '-p' }
   specify { oarg.must_equal '-o name' }
   specify { arg_over.must_equal '-p name' }
+  specify { noshort.must_be_nil }
 end
