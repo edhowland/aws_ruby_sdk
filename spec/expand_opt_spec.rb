7ed649fd06a653891eaed3020ef0ec9af5da3b5b
edhowland <ed.howland@gmail.com>
2015-07-29 22:23:59 +0000

    \'added override option name if options.short set\'

--- spec/expand_opt_spec.rb
+++ spec/expand_opt_spec.rb
@@ -5,11 +5,13 @@ require 'application'
 require 'minitest/autorun'
 
 def expand_short key, options={}
-  "-#{key.to_s[0]}"
+  '-' + (options[:short] || key.to_s[0])
 end
 
 describe 'expand_short' do
   let(:opt) { expand_short :option }
+  let(:override) { expand_short :option, {short: 'p'} }
 
   specify { opt.must_equal '-o' }
+  specify { override.must_equal '-p' }
 end
