0775796c3dcb0c9185a5d930429a936e76943511
edhowland <ed.howland@gmail.com>
2015-07-29 22:28:51 +0000

    \'added name if :arg set in options\'

--- spec/expand_opt_spec.rb
+++ spec/expand_opt_spec.rb
@@ -5,13 +5,15 @@ require 'application'
 require 'minitest/autorun'
 
 def expand_short key, options={}
-  '-' + (options[:short] || key.to_s[0])
+  '-' + (options[:short] || key.to_s[0]) + (options[:arg] ? ' name' : '')
 end
 
 describe 'expand_short' do
   let(:opt) { expand_short :option }
   let(:override) { expand_short :option, {short: 'p'} }
+  let(:oarg) { expand_short :option, {arg: String} }
 
   specify { opt.must_equal '-o' }
   specify { override.must_equal '-p' }
+  specify { oarg.must_equal '-o name' }
 end
