245a8a76cc779ea4c085ca2ea2bd5224e67dada3
edhowland <ed.howland@gmail.com>
2015-07-30 17:56:35 +0000

    \'chkpt set_options_spec.rb\'

--- spec/set_options_spec.rb
+++ spec/set_options_spec.rb
@@ -12,9 +12,13 @@ end
 
 describe 'on_args' do
   let(:cut) { MixedCut.new }
+  let(:noshort) { cut.on_args({description: 'No Short', short: nil, long: '--no-short'}) }
+  let(:nolong) { cut.on_args({description: 'No Long', long: nil, short: '-n'}) }
   subject { cut.on_args({description: 'Long Option', short: '-l', long: '--long-option'}) }
 
   specify { subject.must_equal ['-l', '--long-option', 'Long Option'] } 
+  specify { noshort.must_equal ['--no-short', 'No Short'] }
+  specify { nolong.must_equal ['-n', 'No Long'] }
 end
 
 describe 'set_options'  do
