327f279fb7f558bc637adbfc2bb9a9de224c886f
edhowland <ed.howland@gmail.com>
2015-08-03 19:49:14 +0000

    \'added gen-key.sh ssh-login.sh scripts to automate key generation and logging in to new instance\'

---
+++ bin/virtual_pc.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# virtual_pc.rb - create and manage AWS VPC
+
+
+require_relative '../lib/application'
+require 'json'
+require_relative 'ec2_options'
+require './messages'
+require './requestor'
+require './handle_instance'
+
