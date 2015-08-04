87fbb0d84124ce690d1f35ca54ad0b3dcb55f77f
edhowland <ed.howland@gmail.com>
2015-08-04 01:48:46 +0000

    \'added stub/region.rb and stub/credentials.rb stubs to be filled in by users\'

---
+++ stubs/credentials.rb
@@ -0,0 +1,4 @@
+# credentials.rb - secret kwys for AWS
+# place this in ~/.aws/credentials.rb
+ENV['AWS_ACCESS_KEY_ID'] = ''  # put your host access key here
+ENV['AWS_SECRET_ACCESS_KEY'] = '' # put your secret key here
---
+++ stubs/region.rb
@@ -0,0 +1,5 @@
+# region.rb - loads AWS Region
+# put in ~/.aws/region.rb
+
+# this is not a stub. Use this region
+ENV['AWS_REGION'] = 'us-east-1'
