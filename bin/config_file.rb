81578c0fbc18a93c4b018ce477152d874f7af8ab
edhowland <ed.howland@gmail.com>
2015-08-10 15:48:04 +0000

    \'added config_name to config_file.rb\'

--- bin/config_file.rb
+++ bin/config_file.rb
@@ -9,4 +9,8 @@ class ConfigFile < OptionDecorator
   def config_fname name # {description: 'File name root for config settings', short: 'f', arg: String}
     @my_options[:config_fname] = name
 end
+
+  def config_name name  # {description: 'Set the name of the configuration', arg: String, short: 'n'}
+    @my_options[:config_name] = name
+  end
 end
